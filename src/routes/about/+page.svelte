2abeaeb3dd46b50dd1df494f6a3614fb4f8e8e29
vedantmartin <vedantmartin@gmail.com>
2023-06-28 23:38:20 +0100

    made about consistent

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,19 +1,23 @@
-<!-- this is the about page -->
-<div class="flex flex-col sm:h-screen font-spg w-screen bg-base-100 md:p-10 p-6">
- <h1 class=" font-bold text-3xl p-4 pt-10 text-secondary ">About Me</h1>
- <p class=" text-lg p-4">
- I am an experienced Learning and Development professional with a strong foundation in Educational Technology. I possess a deep understanding of best practices and instructional design principles. I consistently stay updated with emerging trends and technologies in the industry, ensuring innovative and effective solutions. 
- </p>
-<ul class=" list-disc px-8">
-  <li>Proficient in designing and delivering effective learning experiences for diverse audiences, utilizing a variety of delivery methods including online, face-to-face, and blended learning.</li>
+<div class="hero min-h-screen font-spg ">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://media.licdn.com/dms/image/C4D03AQElw-0PlWTulA/profile-displayphoto-shrink_800_800/0/1641389748084?e=2147483647&v=beta&t=1kAvVpBufNtgBQLFZ5cqaE3pHN9i4WtC_nusFzvZS_A" class=" object-cover max-w-sm h-[60vh] rounded-lg shadow-2xl" />
+    <div>
+      <div class="py-6">
+        <p class="text-xl text-secondary p-1 py-2 font-bold">/about</p>
+        <h1 class=" text-5xl lg:text-6xl font-bold pb-4">Get to know more about me</h1>
+      </div>
 
-  <li>Experienced in collaborating with subject matter experts to understand content, learning objectives, and conducting needs assessments to develop impactful and tailored learning solutions.</li>
-  <li>Experienced in designing interactive learning resources, including online tutorials, instructional videos, and interactive PDFs by utilizing authoring tools such as Articulate Storyline and Adobe Captivate.</li>
-<li>Comfortable coordinating with technical teams for seamless course development and implementation.
-Well-versed in learning management systems (LMS) such as Moodle, Canvas, and Blackboard.</li>
-<li>
-Strong facilitation and presentation skills, with experience in conducting workshops, seminars, and virtual trainings using platforms like Microsoft Teams and WebEx.</li>
-</ul>
+      <ul class=" list-disc px-8 text-lg">
+        <li>Proficient in designing and delivering effective learning experiences for diverse audiences, utilizing a variety of delivery methods including online, face-to-face, and blended learning.</li>
 
-
+        <li>Experienced in collaborating with subject matter experts to understand content, learning objectives, and conducting needs assessments to develop impactful and tailored learning solutions.</li>
+        <li>Experienced in designing interactive learning resources, including online tutorials, instructional videos, and interactive PDFs by utilizing authoring tools such as Articulate Storyline and Adobe Captivate.</li>
+        <li>Comfortable coordinating with technical teams for seamless course development and implementation.
+        Well-versed in learning management systems (LMS) such as Moodle, Canvas, and Blackboard.</li>
+        <li>
+        Strong facilitation and presentation skills, with experience in conducting workshops, seminars, and virtual trainings using platforms like Microsoft Teams and WebEx.</li>
+      </ul>
+      <!-- <button class="btn btn-primary">Get Started</button> -->
+    </div>
+  </div>
 </div>
